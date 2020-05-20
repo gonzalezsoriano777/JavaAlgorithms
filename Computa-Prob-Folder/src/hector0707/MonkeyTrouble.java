0fc460b1fd6655d5e43471b0b9d81b00eba11518
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-05-19 20:29:32 -0500

    MonkeyTrouble complete, trouble if either both or neither monkey's are smiling!

--- Computa-Prob-Folder/src/hector0707/MonkeyTrouble.java
+++ Computa-Prob-Folder/src/hector0707/MonkeyTrouble.java
@@ -5,8 +5,21 @@ public class MonkeyTrouble {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World");
+		System.out.println(theMonkeyTrouble(false, false));
 
+	}
+	
+	public static boolean theMonkeyTrouble(boolean aSmile, boolean bSmile) {
+		
+		// If both Monkey's are either smiling or upset... 
+		//it will be considered MonkeyTrouble! ELSE: No MonkeyTrouble
+		
+		if(aSmile && bSmile || !aSmile && !bSmile) {
+			return true;
+		}
+		
+		return false;
+		
 	}
 
 }
