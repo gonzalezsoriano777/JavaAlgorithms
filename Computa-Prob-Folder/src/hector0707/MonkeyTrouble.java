55986fcb9997ba767f4044adc3adabfcff2b1714
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-05-19 19:38:09 -0500

    Finally created and added my java package to my new class called MonkeyTrouble. Fresh Start.

--- Computa-Prob-Folder/src/hector0707/MonkeyTrouble.java
+++ Computa-Prob-Folder/src/hector0707/MonkeyTrouble.java
@@ -1,7 +1,11 @@
+package hector0707;
+
 public class MonkeyTrouble {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
 
 	}
 
