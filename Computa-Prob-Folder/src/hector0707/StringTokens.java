6fa577cc875a0ab6355af4a675533b9b857f269d
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-05-21 20:08:34 -0500

    Finds out the length of the string tokens passed

--- Computa-Prob-Folder/src/hector0707/StringTokens.java
+++ Computa-Prob-Folder/src/hector0707/StringTokens.java
@@ -6,6 +6,8 @@ public class StringTokens {
 	
 		String[] tokens = "He is a very very good boy, isn't he?".split("[ |!|,|\\?|\\.|_|'|@|]+");
 	       
+		System.out.println(tokens.length);
+		
 		for(String token : tokens){
 	       System.out.println(token);
 		
