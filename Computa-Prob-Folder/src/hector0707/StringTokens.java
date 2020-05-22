1c78e0fcf8a97b9164f2eb4c20451e9904a380cd
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-05-21 19:55:58 -0500

    More simpler way to identify and conclude the solution for this problem!

--- Computa-Prob-Folder/src/hector0707/StringTokens.java
+++ Computa-Prob-Folder/src/hector0707/StringTokens.java
@@ -4,24 +4,11 @@ public class StringTokens {
 	
 	public void tokenString() {
 	
-		Scanner sc = new Scanner(System.in);
-        
-        if (! sc.hasNext()) {
-            System.out.println("0");  
-            return;
-        }
-        
-        String line = sc.nextLine().trim();
-        
-        if (line.length() == 0) {
-            System.out.println("0");  
-            return;
-        }
-        
-        String[] sLine = line.split("[!,?._'@ ]+");
-        System.out.println(sLine.length);  
-        for (String s : sLine) 
-            System.out.println(s.trim());   
+		String[] tokens = "He is a very very good boy, isn't he?".split("[ |!|,|\\?|\\.|_|'|@|]+");
+	       
+		for(String token : tokens){
+	       System.out.println(token);
 		
+	    }
 	}
 }
