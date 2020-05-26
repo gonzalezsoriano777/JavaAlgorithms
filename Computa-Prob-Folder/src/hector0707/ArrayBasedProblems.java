b25f104b20e3c1e9573fc135eb522a0b023d0456
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-05-26 12:58:05 -0500

    maxSpan method checks for similar elements and counts the matching ones as one single span element and counts the rest the of the elements and stores and sums it up in the max variable.

--- Computa-Prob-Folder/src/hector0707/ArrayBasedProblems.java
+++ Computa-Prob-Folder/src/hector0707/ArrayBasedProblems.java
@@ -1,5 +1,27 @@
 package hector0707;
 
 public class ArrayBasedProblems {
+	
+	public int maxSpan(int[] nums) {
+		
+		int span = 0;
+		int max = 0;
+		
+		for(int i = 0; i < nums.length; i++) {
+			
+			int j = nums.length - 1;
+			
+			while(nums[i] != nums[j]) 
+				
+				j--;
+				span = j - i + 1;
+			
+			if(span > max) {
+				max = span;
+			}
+		}
+		
+		return max;
+	}
 
 }
