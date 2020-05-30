23635020545cab40b0655baf5695f548315b26f2
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-05-29 21:19:04 -0500

    Able to merge both elements of either 3 & 4 once located in array.

--- Computa-Prob-Folder/src/hector0707/ArrayBasedProblems.java
+++ Computa-Prob-Folder/src/hector0707/ArrayBasedProblems.java
@@ -1,5 +1,7 @@
 package hector0707;
 
+import java.util.ArrayList;
+
 public class ArrayBasedProblems {
 	
 	public int maxSpan(int[] nums) {
@@ -26,7 +28,40 @@ public class ArrayBasedProblems {
 	
 	public int[] fix34(int[] nums) {
 		
-		return nums;
+		 ArrayList<Integer> others = new ArrayList<Integer>();
+
+		    for (int i = 0; i < nums.length; i++ )
+		    {
+		        if ( nums[i] != 3 && nums[i] != 4 )
+		            others.add(nums[i]);
+		    }       
+
+		    int[] result = new int[nums.length];
+
+		   
+		    for ( int i = 0; i < nums.length - 1; i++ )
+		    {
+		        if ( nums[i] == 3)
+		        {
+		            result[i] = 3;
+		            result[i+1] = 4;
+		        }           
+		    }
+
+		    
+		    int j = 0;
+		    for ( int i = 0; i < result.length; i++ )
+		    {
+		        if ( result[i] == 0 )
+		        {               
+		            int temp = others.get(j);
+		            j++;
+		            result[i] = temp;
+		        }
+
+		    }
+
+		    return result;
 		
 	}
 
