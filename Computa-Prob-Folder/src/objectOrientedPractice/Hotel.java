bba4cac55d229266ed58cc5abfc09ef840f2f7ce
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-11-05 19:18:06 -0600

    Created a mini Hotel App that included room availability and check in check out process.

--- Computa-Prob-Folder/src/objectOrientedPractice/Hotel.java
+++ Computa-Prob-Folder/src/objectOrientedPractice/Hotel.java
@@ -1,10 +1,18 @@
 package objectOrientedPractice;
 
+import java.util.ArrayList;
+
 public class Hotel {
 	
-	public void Hotel() {
+	private ArrayList<Room> rooms;
+	
+	public Hotel(ArrayList<Room> rooms) {
+		this.rooms = rooms;
+	}
+	
+	public ArrayList<Room> displayableRoom(){
 		
-		System.out.println("Hotel in construction ");
+		return rooms;
 		
 	}
 
