44fb9b889970510505e314d9a220e641dcb217ed
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-11-05 23:51:16 -0600

    Finishing up the process.

--- Computa-Prob-Folder/src/objectOrientedPractice/Main.java
+++ Computa-Prob-Folder/src/objectOrientedPractice/Main.java
@@ -1,11 +1,24 @@
 package objectOrientedPractice;
 
 class Main {
+	
+	// Init. Hotel && Num of rooms
+	static Hotel hotel = new Hotel("Hotel del pauir", 20);
+	static int numOfRooms = hotel.findAllNumRooms();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		hotelProcess();
 		
+	}
+	
+	public static void hotelProcess() {
+		
+		System.out.println("Hi there! Welcome to " + hotel.getHotelName() + " !");
+		System.out.println();
+		System.out.println();
+
 		
 	}
 
