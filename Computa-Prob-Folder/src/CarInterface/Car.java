989efd5e01d0a8c17090596cc8dee42a93d8184e
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-11-09 19:02:59 -0600

    Main ready

--- Computa-Prob-Folder/src/CarInterface/Car.java
+++ Computa-Prob-Folder/src/CarInterface/Car.java
@@ -1,15 +1,19 @@
 package CarInterface;
 
 public class Car {
-	
+
 	private int Mileage;
 	private String Engine;
 	private int Speed;
 	private int Year;
-	
+
 	public Car(int mileage, String engine, int speed, int year) {
-		
+		this.Mileage = mileage;
+		this.Engine = engine;
+		this.Speed = speed;
+		this.Year = year;
 	}
+	
 	
 
 }
